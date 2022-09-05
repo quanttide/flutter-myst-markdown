0a528474970d24a51f872930e304b6e0867b4d4a
Guo Zhang <zhangguo@quanttide.com>
2022-09-05 15:41:30 +0800

    feature: add CodeExecutingController

--- lib/src/widgets/executive_code_block.dart
+++ lib/src/widgets/executive_code_block.dart
@@ -1,23 +1,57 @@
 import "package:flutter/material.dart";
+import "package:provider/provider.dart";
+
+
+/// Controller for executive code block
+///
+/// ```dart
+/// controller = CodeExecutingController(
+///   codeExecutingHandler: mockFunc,
+/// )
+/// ```
+///
+/// ref:
+///   - https://docs.flutter.dev/development/data-and-backend/state-mgmt/simple#changenotifier
+///   - https://api.flutter.dev/flutter/foundation/ValueNotifier-class.html
+///   - https://github.com/flutter/plugins/blob/main/packages/video_player/video_player/lib/video_player.dart
+class CodeExecutingController extends ValueNotifier<String> {
+  final Function codeExecutingHandler;
+  final String defaultOutput;
+
+  CodeExecutingController({
+    required this.codeExecutingHandler,
+    this.defaultOutput = ''
+  }) : super(defaultOutput);
+
+  Future<void> execute(String input) async {
+    value = await codeExecutingHandler(input);
+    notifyListeners();
+  }
+}
 
 
 /// Widget for executive code block
 class ExecutiveCodeBlock extends StatelessWidget {
+  final CodeExecutingController codeExecutingController;
   final String? defaultInput;
 
   const ExecutiveCodeBlock({
     super.key,
+    required this.codeExecutingController,
     this.defaultInput
   });
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const ExecutiveCodeControlPanel(),
-        ExecutiveCodeInput(defaultInput: defaultInput),
-        const ExecutiveCodeOutput(),
-      ]
+    return ChangeNotifierProvider(
+      create: (context) => codeExecutingController,
+      child: Column(
+          children: [
+            const ExecutiveCodeControlPanel(),
+            ExecutiveCodeInput(defaultInput: defaultInput),
+            const ExecutiveCodeOutput(),
+          ]
+      )
     );
   }
 }
@@ -37,7 +71,7 @@ class ExecutiveCodeControlPanel extends StatelessWidget {
 
 /// Show the input of executive code block,
 /// allow user to input its code.
-class ExecutiveCodeInput extends StatefulWidget {
+class ExecutiveCodeInput extends StatelessWidget {
   final String? defaultInput;
 
   const ExecutiveCodeInput({
@@ -46,23 +80,9 @@ class ExecutiveCodeInput extends StatefulWidget {
   });
 
   @override
-  State<StatefulWidget> createState() => ExecutiveCodeInputState();
-
-}
-
-class ExecutiveCodeInputState extends State<ExecutiveCodeInput> {
-  late TextEditingController textEditingController;
-
-  @override
-  void initState() {
-    super.initState();
-    textEditingController = TextEditingController(text: widget.defaultInput);
-  }
-
-  @override
   Widget build(BuildContext context) {
     return EditableText(
-      controller: textEditingController,
+      controller: TextEditingController(text: defaultInput),
       focusNode: FocusNode(),
       style: const TextStyle(),
       cursorColor: Colors.green,
@@ -70,26 +90,19 @@ class ExecutiveCodeInputState extends State<ExecutiveCodeInput> {
       maxLines: null,  // default is 1
     );
   }
-
-  @override
-  void dispose(){
-    textEditingController.dispose();
-    super.dispose();
-  }
 }
 
 
 /// Show the output of executive code block
-class ExecutiveCodeOutput extends StatefulWidget {
+class ExecutiveCodeOutput extends StatelessWidget {
   const ExecutiveCodeOutput({super.key});
 
   @override
-  State<StatefulWidget> createState() => ExecutiveCodeOutputState();
-}
-
-class ExecutiveCodeOutputState extends State<ExecutiveCodeOutput>{
-  @override
   Widget build(BuildContext context) {
-    return const Text('');
+    return Consumer<CodeExecutingController>(
+      builder: (context, controller, child){
+        return const Text('');
+      }
+    );
   }
 }
--- test/executive_code_block_test.dart
+++ test/executive_code_block_test.dart
@@ -2,9 +2,30 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:markdown/markdown.dart';
 
 import 'package:flutter_myst_markdown/flutter_myst_markdown.dart';
+import 'mock.dart';
 
 
 void main(){
+  group('CodeExecutingController', (){
+    // https://docs.flutter.dev/development/data-and-backend/state-mgmt/simple#changenotifier
+    test('CodeExecutingController.init', (){
+      final controller = CodeExecutingController(
+        codeExecutingHandler: mockCodeExecutingHandler,
+        defaultOutput: "defaultOutput"
+      );
+      expect(controller.value, "defaultOutput");
+    });
+    test('CodeExecutingController.execute', (){
+      final controller = CodeExecutingController(
+          codeExecutingHandler: mockCodeExecutingHandler
+      );
+      String input = "print('Hello, world')";
+      controller.addListener(() {
+        expect(controller.value, input);
+      });
+      controller.execute(input);
+    });
+  });
   group('ExecutiveCodeOutput', () {
     test('ExecutiveCodeOutput.init', () {
 
